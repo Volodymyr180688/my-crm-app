3871b27ad080177abe93b5a078841d67ac2a964f
Volodymyr180688 <shurik180688@gmail.com>
2024-10-13 15:01:17 +0300

    add intercepting routes for companies

--- src/app/(admin)/companies/layout.tsx
+++ src/app/(admin)/companies/layout.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 export interface LayoutProps {
-    children: React.ReactNode;
-    header: React.ReactNode;
-    toolbar: React.ReactNode;
-    modal: React.ReactNode;
+  children: React.ReactNode;
+  header: React.ReactNode;
+  toolbar: React.ReactNode;
+  modal: React.ReactNode;
 }
 export default function Layout({
-                                   children,
-                                   header,
-                                   toolbar,
-                                   modal,
-                               }: LayoutProps) {
-    return (
-        <>
-            {modal}
-            {header}
-            <main>
-                {toolbar}
-                {children}
-            </main>
-        </>
-    );
+  children,
+  header,
+  toolbar,
+  modal,
+}: LayoutProps) {
+  return (
+    <>
+      {modal}
+      {header}
+      <main>
+        {toolbar}
+        {children}
+      </main>
+    </>
+  );
 }
